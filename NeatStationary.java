c0eef69d7c7b201962e91a2bfee4f06b532f3cab
Godwin365-cent <godwinmodii@gmail.com>
2026-08-28 17:32:25 +0300

    i have finalised the receipt

--- NeatStationary.java
+++ NeatStationary.java
@@ -19,5 +19,32 @@ public class NeatStationary{
             System.out.println(names[i] + " - UGX " 
                              + String.format("%.2f", prices[i]));
 
+        double grandTotal = 0.0;
+        for (int i = 0; i < names.length; i++) {
+            subtotals[i] = prices[i] * quantities[i];
+            if (names[i].equals("Pen") && quantities[i] >= 10) {
+                subtotals[i] *= 0.90;
+                discounts[i] = "10% off total";
+            } else if (names[i].equals("Ruler") && quantities[i] >= 5) {
+                subtotals[i] -= 200.00;
+                discounts[i] = "UGX 200 off total";
+            } else if (names[i].equals("Maths Set") && quantities[i] >= 3) {
+                subtotals[i] *= 0.95;
+                discounts[i] = "5% off total";
             }
+            grandTotal += subtotals[i];
         }
+        System.out.println("==== RECEIPT ====");
+        System.out.println("Item - Quantity - Discount - Subtotal");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " x " + quantities[i]
+                             + " - " + discounts[i]
+                             + " - UGX " + String.format("%.2f", subtotals[i]));
+        }
+        System.out.println("==== GRAND TOTAL ====");
+        System.out.println("UGX " + String.format("%.2f", grandTotal));
+
+
+    }
+
+}
